4201d261f0851cbbcdb0e0cc8a5a2e19ad1a87f4
Walter Tiberti <wtuniv@gmail.com>
2012-09-22 07:49:17 +0200

    Added licensing information file and file headers

--- ElfHunterMainWidget.cpp
+++ ElfHunterMainWidget.cpp
@@ -1,3 +1,28 @@
+/*
+* This file is part of ElfHunter software.
+*
+* ElfHunter is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ElfHunter is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with ElfHunter.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright (C) 2012 Walter Tiberti
+*/
+
+/*
+* Filename: ElfHunterMainWidget.cpp
+* Author: Walter Tiberti <wtuniv@gmail.com>
+*
+*/
+
 #include "ElfHunterMainWidget.h"
 
 #ifndef ELFHUNTER_STANDALONE
